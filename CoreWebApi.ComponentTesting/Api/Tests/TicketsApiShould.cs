951512d8e89ca00402befc5bca7d1892371ce0f0
Robert Sebescen <robert.sebescen@devtechgroup.com>
2018-06-21 16:48:22 +0200

    added first tests

---
+++ CoreWebApi.ComponentTesting/Api/Tests/TicketsApiShould.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Api;
+using Api.Configuration;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Tests
+{
+    public class TicketsApiShould : IClassFixture<ApiFixture>
+    {
+        private readonly ApiFixture _fixture;
+        static HttpClient client;
+
+        public TicketsApiShould(ApiFixture fixture)
+        {
+            _fixture = fixture;
+            client = new HttpClient(_fixture.Handler);
+            client.BaseAddress = new Uri("http://localhost:5000");
+        }
+
+        [Fact]
+        public async Task CreateTicketAndTask()
+        {
+            var request = @"
+            {
+                ""type"":""Normal"",
+                ""text"":""Something"",
+            }
+            ";
+
+            HttpResponseMessage response = await client.PostAsync(
+                "api/tickets", new StringContent(request, Encoding.Unicode, "application/json"));
+            response.EnsureSuccessStatusCode();
+        }
+    }
+
+    public class ApiFixture : IDisposable
+    {
+        private TestServer _server;
+
+        public HttpMessageHandler Handler;
+
+        public ApiFixture()
+        {
+            var builder = new WebHostBuilder()
+                .UseStartup<Startup>()
+                .ConfigureServices(configureServices =>
+                {
+                    configureServices.AddSingleton(new MongoConfiguration
+                    {
+                        ConnectionString = "mongodb://localhost:27017",
+                        Database = "Api"
+                    });
+                    configureServices.AddSingleton(new TasksConfiguration
+                    {
+                        BaseUrl = "http://localhost:5000"
+                    });
+                });
+            _server = new TestServer(builder);
+            Handler = _server.CreateHandler();
+        }
+
+        public void Dispose()
+        {
+            Handler.Dispose();
+            _server.Dispose();
+        }
+    }
+}
